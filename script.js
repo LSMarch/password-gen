99d911dc6dcb249e381d9edca1816513ad41aaa9
Libby Marchetti <lsmarchetti01@gmail.com>
2022-03-22 18:39:11 -0500

    building script

--- script.js
+++ script.js
@@ -6,8 +6,10 @@
 console.log("Here we go!");
 // password length prompt
 // var passLength = prompt("Choose a number between 8-128");
-
-
+var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
+var upperChar = "ABCDEFGHIJKLMNOPQRSTYVWXYZ";
+var numChar = 1234567890;
+//var specialChar = "!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -16,23 +18,49 @@ var generateBtn = document.querySelector("#generate");
 
 
 function writePassword() {
+  alert("Let's make a password!")
   // password length prompt
 var passLength = prompt("Choose a number between 8-128");
   if(passLength < 8){
     alert("Password must be at least 8 characters");
   } else if(passLength > 128) {
     alert("Password must be no more than 128 character");
-  } 
+  } else {
+  
+    //input acceptable length, move to confirms (unnest??)
+    var lowerConfirm = prompt("Include lowercase? Type yes or no");
+      if(lowerConfirm ==="yes"||"Yes"||"YES") {
+        lowerConfirm = lowerChar;
+      } 
+    var upperConfirm = prompt("Include uppercase? Type yes or no");
+      if(upperConfirm === "yes"||"Yes"||"YES") {
+        upperConfirm = upperChar;        
+      }
+    var numConfirm = prompt("Include numbers? Type yes or no")
+      if(numConfirm === "yes" || "Yes"||"YES") {
+        numConfirm = numChar
+      }
+    var specialConfirm = prompt("Include special characters? Type yes or no");
+      if(specialConfirm === "yes"||"Yes"||"YES") {
+        specialConfirm = specialChar
+      }
+  
+      //confrim("Okay, next question. Include uppercase?")
+      }
+      //var upperChar = confirm("Include uppercase?");   
+  }
 
   //character type confirms
+  //var lowerChar = confrim("Include lowercase?");
+
   
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ // var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ // passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
